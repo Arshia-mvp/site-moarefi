c9b21b86d9e79f581517a2fdbf114d031d097232
Arshia-Zahedi <158775813+Arshia-mvp@users.noreply.github.com>
2024-03-07 14:30:49 +0330

    Add files via upload

---
+++ site moarefi/js/app.js
@@ -0,0 +1,54 @@
+alert('hello everyone');
+let daysVick = (1);
+switch(daysVick){
+    case 1:
+        alert('امروز پنجشنبه است');
+    break;
+}
+alert('i hope you are will good');
+let userName1 = prompt('whats your name');
+alert('hello '+ userName1);
+alert('i hope you will good');
+let userAge = prompt('how old are you');
+if(userAge < 25){
+    alert('اوه هنووز جوونی ها');
+}
+else if(userAge > 25){
+    alert('اوه داری پیر میشی ها');
+}
+else if(userAge = 25){
+    alert('دمت گرم مشتی هستی ها');
+}
+let userNumber1 = Number(prompt('لطفا یک شماره تلفن از خودت بده'));
+alert('خوبه،ازت ممنونم که داری از سایت ما استفاده میکنی');
+let userName2 = prompt('enter your email');
+let userNumber2 = Number(prompt('enter your password'));
+alert('خوبه ازت ممنونم');
+alert('اگر از سایت ما خوشت اومد به ما ستاره بده و اگر خوشت نیومد به ما ستاره نده');
+let userNumber3 = Number(prompt('لطفا به ما از 1 تا 5 ستاره بده'));
+if(userNumber3 == 0){
+    result = userNumber3
+    alert('Na motabar');
+}
+else if(userNumber3 == 1){
+    result = userNumber3
+    alert('خیلی بده');
+}
+else if(userNumber3 == 2){
+    result = userNumber3
+    alert('بده');
+}
+else if(userNumber3 == 3){
+    result = userNumber3
+    alert('خوبه');
+}
+else if(userNumber3 == 4){
+    result = userNumber3
+    alert('خیلی خوبه');
+}
+else if(userNumber3 == 5){
+    result = userNumber3
+    alert('عالیه');
+}
+alert('خیلی ازتون ممنونم که از سایت ما استفاده کردید');
+alert('با تشکر و سپاس از همتون');
